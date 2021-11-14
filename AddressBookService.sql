237e9739262beba044c42af43ff355e0a2bf3608
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-14 19:23:48 +0530

    Insert contact details to address book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -9,3 +9,9 @@ use address_book_service;
 use address_book_service;
 create table aadress_book(first_name varchar(250), last_name varchar(250), address varchar(250), city varchar(250), state varchar(250), zip char(10), phone_number char(10), email varchar(120));
 desc aadress_book;
+
+-- UC3
+insert into aadress_book values('Praphulla', 'Khearde', 'Bhandup', 'Mumbai', 'Maharashtra', 400078, 8104703089, 'prafullkherade9@gmail.com');
+insert into aadress_book values('Sayali', 'Thakur', 'Chiplun', 'Ratnagiri', 'Maharashtra', 415722, 8704583785, 'thakursayali@gmail.com');
+insert into aadress_book values('Virat', 'Kohli', 'Mathura Road', 'Narela', 'Delhi', 110054, 9104458389, 'viratkohli18@gmail.com');
+select * from aadress_book;
